da4b72347f3698aa9fa6b44f6ad63e35c02c1bc4
yj90 <ysjamdhade17@gmail.com>
2023-09-07 03:24:36 +0530

    first commit for workflows-GitHubActions

--- utils.py
+++ utils.py
@@ -1,55 +1,71 @@
-# Import datasets, classifiers and performance metrics
-# Import datasets, classifiers, and performance metrics
-from sklearn import svm, datasets, metrics  # Added 'metrics' import
 from sklearn.model_selection import train_test_split
+from sklearn import svm, datasets, metrics
+# we will put all utils here
 
-# Read digits
+
+
 def read_digits():
     digits = datasets.load_digits()
-    x = digits.images
+    X = digits.images
     y = digits.target
-    return x, y
+    return X, y 
 
-# We will define utils here:
 def preprocess_data(data):
+    # flatten the images
     n_samples = len(data)
     data = data.reshape((n_samples, -1))
     return data
 
-# Split data into train, test, and dev subsets
-def split_data(X, y, test_size=0.5, dev_size=0.2, random_state=1):
-    X_train, X_temp, y_train, y_temp = train_test_split(
-        X, y, test_size=test_size + dev_size, random_state=random_state
+# Split data into 50% train and 50% test subsets
+def split_data(x, y, test_size, random_state=1):
+    X_train, X_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.5,random_state=random_state
     )
-    X_test, X_dev, y_test, y_dev = train_test_split(
-        X_temp, y_temp, test_size=dev_size / (test_size + dev_size), random_state=random_state
-    )
-    return X_train, X_test, X_dev, y_train, y_test, y_dev
+    return X_train, X_test, y_train, y_test
 
-# Create a classifier: a support vector classifier
-def train_model(X, y, model_params, model_type='svm'):
-    if model_type == 'svm':
-        clf = svm.SVC(**model_params)
-    clf.fit(X, y)
-    return clf
+# train the model of choice with the model prameter
+def train_model(x, y, model_params, model_type="svm"):
+    if model_type == "svm":
+        # Create a classifier: a support vector classifier
+        clf = svm.SVC
+    model = clf(**model_params)
+    # train the model
+    model.fit(x, y)
+    return model
 
-# Question1: Test, Train, Dev Split
-def test_train_dev_split():
-    # Read digits
-    x, y = read_digits()
-    
-    # Split data into train, test, and dev subsets
-    X_train, X_test, X_dev, y_train, y_test, y_dev = split_data(x, y, test_size=0.3, dev_size=0.2)
-    
-    return X_train, X_test, X_dev, y_train, y_test, y_dev
 
-# Question2: Predict and Evaluate
+def train_test_dev_split(X, y, test_size, dev_size):
+    X_train_dev, X_test, Y_train_Dev, y_test =  train_test_split(X, y, test_size=test_size, random_state=1)
+    X_train, X_dev, y_train, y_dev = split_data(X_train_dev, Y_train_Dev, dev_size/(1-test_size), random_state=1)
+    return X_train, X_test, y_train, y_test, X_dev, y_dev
+
+# Question 2:
 def predict_and_eval(model, X_test, y_test):
-    # Model Prediction
     predicted = model.predict(X_test)
-    
-    # Model Evaluation
-    report = metrics.classification_report(y_test, predicted)
-    confusion_matrix = metrics.confusion_matrix(y_test, predicted)
-    
-    return predicted, report, confusion_matrix
+    print(
+    f"Classification report for classifier {model}:\n"
+    f"{metrics.classification_report(y_test, predicted)}\n"
+    )
+
+
+    disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
+    disp.figure_.suptitle("Confusion Matrix")
+    print(f"Confusion matrix:\n{disp.confusion_matrix}")
+
+
+    # The ground truth and predicted lists
+    y_true = []
+    y_pred = []
+    cm = disp.confusion_matrix
+
+    # For each cell in the confusion matrix, add the corresponding ground truths
+    # and predictions to the lists
+    for gt in range(len(cm)):
+        for pred in range(len(cm)):
+            y_true += [gt] * cm[gt][pred]
+            y_pred += [pred] * cm[gt][pred]
+
+    print(
+        "Classification report rebuilt from confusion matrix:\n"
+        f"{metrics.classification_report(y_true, y_pred)}\n"
+    )
