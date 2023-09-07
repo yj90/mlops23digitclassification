998e3e30523c2edfa9b85dd263e68b28d4aff22d
yj90 <ysjamdhade17@gmail.com>
2023-09-07 08:58:33 +0530

    assignment 3 work

--- exp.py
+++ exp.py
@@ -1,64 +1,33 @@
-"""
-================================
-Recognizing hand-written digits
-================================
-
-This example shows how scikit-learn can be used to recognize images of
-hand-written digits, from 0-9.
-
-"""
-
-# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
-# License: BSD 3 clause
-
-# Import datasets, classifiers and performance metrics
-from sklearn import metrics, svm
-from utils import preprocess_data, split_data, train_model, read_digits, predict_and_eval, train_test_dev_split
+from sklearn import metrics
+from utils import preprocess_data, train_test_dev_split, tune_hparams, read_digits, predict_and_eval
+import itertools
 
 # 1. Get the dataset
 X, y = read_digits()
 
 # 3. Data splitting -- to create train and test sets
+test_sizes = [0.1, 0.2, 0.3]
+dev_sizes = [0.1, 0.2, 0.3]
 
-# X_train, X_test, y_train, y_test = split_data(X,y, test_size=0.3)
-X_train, X_test, X_dev, y_train, y_test, y_dev = train_test_dev_split(X, y, test_size=0.3, dev_size=0.2)
-# 4. Data preprocessing
-X_train = preprocess_data(X_train)
-X_test = preprocess_data(X_test)
-X_dev = preprocess_data(X_dev)
+for test_size, dev_size in itertools.product(test_sizes, dev_sizes):
+    print(f"test_size={test_size} dev_size={dev_size} train_size={1 - test_size - dev_size}", end=' ')
+    
+    X_train, X_test, X_dev, y_train, y_test, y_dev = train_test_dev_split(X, y, test_size=test_size, dev_size=dev_size)
+    
+    # 4. Data preprocessing
+    X_train = preprocess_data(X_train)
+    X_test = preprocess_data(X_test)
+    X_dev = preprocess_data(X_dev)
 
-# HYPER PARAMETER TUNING
-# take all combinations of gamma and C
-gamma_ranges = [0.001, 0.01, 0.1, 1, 10, 100]
-C_ranges = [0.1, 1, 2, 5, 10]
-best_acc_so_far=-1
-best_model=None
-for cur_gamma in gamma_ranges:
-    for cur_C in C_ranges:
-        #print("Running for gamma={} C={}".format(cur_gamma, cur_C))
-        # train model with cur_gamma and cur_C
-        # 5. Model training
-        cur_model = train_model(X_train, y_train, {'gamma': cur_gamma, "C": cur_C}, model_type="svm")
-        # get some performance metric on DEV set
-        cur_accuracy = predict_and_eval(cur_model, X_dev, y_dev)
-        # select the hparam that yield the best performance on DEV set
-        if cur_accuracy > best_acc_so_far:
-            print("New best accuracy:", cur_accuracy)
-            best_acc_so_far = cur_accuracy
-            optimal_gamma = cur_gamma
-            optimal_C = cur_C
-            best_model = cur_model
+    # HYPERPARAMETER TUNING
+    # Create a list of tuples with all combinations of gamma and C
+    gamma_ranges = [0.001, 0.01, 0.1, 1, 10, 100]
+    C_ranges = [0.1, 1, 2, 5, 10]
+    param_combinations = list(itertools.product(gamma_ranges, C_ranges))
 
+    best_hparams, best_model, best_dev_acc, best_test_acc, best_train_acc = tune_hparams(X_train, y_train, X_dev, y_dev, X_test, y_test, param_combinations)
 
-print("Optimal parameters gamma:", optimal_gamma, "C: ", optimal_C)
-
-'''
-# 5. Model training
-model = train_model(X_train, y_train, {'gamma': optimal_gamma, "C":optimal_C}, model_type="svm")
-'''
-
-# 6. Getting model predictions on test set
-# 7. Qualitative sanity check of the predictions
-# 8. Evaluation
-test_acc = predict_and_eval(best_model, X_test, y_test)
-print("Test accuracy:", test_acc)
+    print(f"dev_acc={best_dev_acc:.2f} test_acc={best_test_acc:.2f} train_acc={best_train_acc:.2f}")
+    
+    # Print the best hyperparameters found
+    print(f"Best hyperparameters: gamma={best_hparams['gamma']}, C={best_hparams['C']}\n")
--- utils.py
+++ utils.py
@@ -1,8 +1,5 @@
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, ParameterGrid
 from sklearn import svm, datasets, metrics
-# we will put all utils here
-
-
 
 def read_digits():
     digits = datasets.load_digits()
@@ -11,36 +8,48 @@ def read_digits():
     return X, y 
 
 def preprocess_data(data):
-    # flatten the images
     n_samples = len(data)
     data = data.reshape((n_samples, -1))
     return data
 
-# Split data into 50% train and 50% test subsets
 def split_data(x, y, test_size, random_state=1):
     X_train, X_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.5,random_state=random_state
+        x, y, test_size=0.5, random_state=random_state
     )
     return X_train, X_test, y_train, y_test
 
-# train the model of choice with the model prameter
 def train_model(x, y, model_params, model_type="svm"):
     if model_type == "svm":
-        # Create a classifier: a support vector classifier
-        clf = svm.SVC
-    model = clf(**model_params)
-    # train the model
+        clf = svm.SVC(**model_params)
+    model = clf
     model.fit(x, y)
     return model
 
+def tune_hparams(X_train, y_train, X_dev, y_dev, X_test, y_test, param_combinations):
+    best_acc_so_far = -1
+    best_model = None
+    best_hparams = None
+    best_test_acc = -1
+    best_train_acc = -1
+    for param_combination in param_combinations:
+        model_params = {"gamma": param_combination[0], "C": param_combination[1]}
+        model = train_model(X_train, y_train, model_params)
+        cur_dev_accuracy = predict_and_eval(model, X_dev, y_dev)
+        cur_test_accuracy = predict_and_eval(model, X_test, y_test)
+        cur_train_accuracy = predict_and_eval(model, X_train, y_train)
+        if cur_dev_accuracy > best_acc_so_far:
+            best_acc_so_far = cur_dev_accuracy
+            best_model = model
+            best_hparams = model_params
+            best_test_acc = cur_test_accuracy
+            best_train_acc = cur_train_accuracy
+    return best_hparams, best_model, best_acc_so_far, best_test_acc, best_train_acc
 
 def train_test_dev_split(X, y, test_size, dev_size):
-    X_train_dev, X_test, Y_train_Dev, y_test =  train_test_split(X, y, test_size=test_size, random_state=1)
+    X_train_dev, X_test, Y_train_Dev, y_test = train_test_split(X, y, test_size=test_size, random_state=1)
     X_train, X_dev, y_train, y_dev = split_data(X_train_dev, Y_train_Dev, dev_size/(1-test_size), random_state=1)
     return X_train, X_test, X_dev, y_train, y_test, y_dev
 
-# Question 2:
 def predict_and_eval(model, X_test, y_test):
     predicted = model.predict(X_test)
-    
     return metrics.accuracy_score(y_test, predicted)
